8f219ad0c40f7e59b4ef41948c4c4f2ee924f529
Nesh98 <113505807+Nesh98@users.noreply.github.com>
2022-11-10 16:29:00 +0100

    Add files via upload

---
+++ ex00.js
@@ -0,0 +1,10 @@
+/* Ecrivez une fonction substract qui prend deux paramètres et qui retourne la soustraction des deux paramètres. Votre fonction devra se nommer substract */
+
+function substract(a,b){
+return a - b;
+}
+
+// Ne touchez pas les lignes en dessous.
+module.exports = {
+  substract
+}
