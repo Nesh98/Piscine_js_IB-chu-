6493c241f00d0eacc047e4882b1dc03a9068152a
Nesh98 <113505807+Nesh98@users.noreply.github.com>
2022-11-10 17:47:05 +0100

    Add files via upload

---
+++ ex09.js
@@ -0,0 +1,35 @@
+// ChoomChoom
+
+/* Ecrivez une fonction choomChoom qui prend en paramètre un nombre entier et qui retourne la chaîne de caractère Choom si le nombre est pair et ChoomChoom si le nombre est impair */
+
+/* exemple : nombre 4 ===> "Choom"
+              nombre 5 ===> "ChoomChoom"    */
+function choomChoom(a){
+ 
+if(a % 2 == 0 ){
+  return "Choom"
+} else {
+  return"ChoomChoom"
+}
+  
+  /*console.log("Enter your number")
+const num = prompt ("enter a number");
+if (num % 2 == 0) {
+console.log("Choom")
+ } else { 
+  console.log("ChoomChoom")
+   }
+
+  */
+
+  }
+
+
+
+
+
+
+// Ne touchez pas les lignes en dessous
+module.exports = {
+  choomChoom
+}
