53ed05f1253adce694ba3754b2eb4704f9b599d6
Nesh98 <113505807+Nesh98@users.noreply.github.com>
2022-11-10 17:46:00 +0100

    Add files via upload

---
+++ ex06.js
@@ -0,0 +1,23 @@
+/* Créez un programme qui affiche uniquement les nombres pairs de 2 à 100, à l'aide d'une variable */
+
+// Testez votre programme sur index.js
+for(let i = 0; i <= 100; i = i + 2){
+console.log (i); 
+ }
+
+
+
+
+
+
+/* la console doit afficher  :
+
+2
+4
+6
+8
+10
+...
+98
+100
+*/
